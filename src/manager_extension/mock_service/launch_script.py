b89f0e3520b6aed1f8a00b04f0782f89db8b823a
katelocate <katelocat3@gmail.com>
2024-01-09 11:28:13 +0100

    Fix uvicorn host again...

--- src/manager_extension/mock_service/launch_script.py
+++ src/manager_extension/mock_service/launch_script.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 def main():
-    uvicorn.run("manager_extension.mock_service.main:app", port=8000, log_level="info")
+    uvicorn.run("manager_extension.mock_service.main:app", host='0.0.0.0', port=8000, log_level="info")
